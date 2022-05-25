caf0d28d6fb668b6db70c4f408e86fda504b1311
ajarombek <ajarombek95@gmail.com>
2022-05-24 22:02:32 -0400

    more process creation work

--- process_management/process_management.c
+++ process_management/process_management.c
@@ -2,11 +2,66 @@
 // Author: Andrew Jarombek
 // Date: 5/22/2022
 
-#include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 int main() {
     pid_t pid = getgid();
-    printf("The current process id is: %d", pid);
+    printf("The current process id is: %d\n", pid);
+
+    pid = fork();
+
+    if (pid > 0) {
+        printf("Parent process of child with id %d\n", pid);
+
+        char* args[] = {"ls", NULL};
+        int ret = execvp("ls", args);
+
+        if (ret == -1) {
+            perror("execvp");
+            return 1;
+        }
+    } else if (!pid) {
+        printf("Child process\n");
+
+        int ret = execlp("pwd", "pwd", NULL);
+
+        if (ret == -1) {
+            perror("execlp");
+            exit(EXIT_FAILURE);
+        }
+
+        exit(EXIT_SUCCESS);
+    } else {
+        perror("fork");
+        return 1;
+    }
+
+    int status;
+    pid = wait(&status);
+
+    if (pid == -1) {
+        perror("wait");
+        return 1;
+    }
+
+    if (WIFEXITED(status)) {
+        printf("Child process terminated normally with exit status %d\n", WEXITSTATUS(status));
+    }
+
+    if (WIFSIGNALED(status)) {
+        printf("Child process killed by signal %d %s\n", WTERMSIG(status), WCOREDUMP(status) ? "(dumped core)" : "");
+    }
+
+    if (WIFSTOPPED(status)) {
+        printf("Child process stopped by signal %d\n", WSTOPSIG(status));
+    }
+
+    if (WIFCONTINUED(status)) {
+        printf("Child process continued\n");
+    }
+
+    return 0;
 }
