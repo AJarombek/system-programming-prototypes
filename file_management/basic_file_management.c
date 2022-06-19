28d72b921e774d56ce44dddc0f6decfa04e9eac2
ajarombek <ajarombek95@gmail.com>
2022-06-18 21:23:14 -0400

    more file management work

--- file_management/basic_file_management.c
+++ file_management/basic_file_management.c
@@ -7,6 +7,44 @@
 #include <unistd.h>
 #include <stdio.h>
 
+void filetype(char* filename) {
+    struct stat sb;
+    int ret = stat(filename, &sb);
+
+    if (ret) {
+        perror("stat");
+        return;
+    }
+
+    printf("%s file type: ", filename);
+    switch (sb.st_mode & S_IFMT) {
+        case S_IFBLK:
+            printf("block device node\n");
+            break;
+        case S_IFCHR:
+            printf("character device node\n");
+            break;
+        case S_IFDIR:
+            printf("directory\n");
+            break;
+        case S_IFIFO:
+            printf("fifo\n");
+            break;
+        case S_IFLNK:
+            printf("symbolic link\n");
+            break;
+        case S_IFREG:
+            printf("regular file\n");
+            break;
+        case S_IFSOCK:
+            printf("socket\n");
+            break;
+        default:
+            printf("unknown\n");
+            break;
+    }
+}
+
 int main() {
     struct stat sb;
     int ret = stat("workout_plan.csv", &sb);
@@ -33,6 +71,31 @@ int main() {
 
     gid_t st_gid = sb.st_gid;
     printf("Group ID of file owner: %d\n", st_gid);
+
+    dev_t st_rdev = sb.st_rdev;
+    printf("Device ID of file: %d\n", st_rdev);
+
+    off_t st_size = sb.st_size;
+    printf("Size of file in bytes: %llu\n", st_size);
+
+    blksize_t st_blksize = sb.st_blksize;
+    printf("Preferred block size for I/O on the file: %d\n", st_blksize);
+
+    blkcnt_t st_blocks = sb.st_blocks;
+    printf("Number of blocks allocated for the file: %llu\n", st_blocks);
+
+    printf("Last access time of the file: %ld\n", sb.st_atime);
+    printf("Last modification time of the file: %ld\n", sb.st_mtime);
+    printf("Last status change time of the file: %ld\n", sb.st_ctime);
+
+    // regular file
+    filetype("workout_plan.csv");
+
+    // directory
+    filetype("..");
+
+    // symbolic link
+    filetype("workout");
 
     return 0;
 }
--- file_management/run.sh
+++ file_management/run.sh
@@ -4,4 +4,8 @@
 # Author: Andrew Jarombek
 # Date: 6/14/2022
 
+# Create a symbolic link & verify its creation
+ln -s workout_plan.csv workout
+ls -l workout
+
 bash -c ./basic_file_management
