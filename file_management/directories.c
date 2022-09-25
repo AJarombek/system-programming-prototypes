c9a5f6804036b796ecd5beaae3ac77df49c90fd6
ajarombek <ajarombek95@gmail.com>
2022-09-24 22:00:15 -0400

    more directories work, new github action jobs

--- file_management/directories.c
+++ file_management/directories.c
@@ -4,14 +4,56 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+void print_cwd();
 
 int main() {
+    print_cwd();
+
+    // Switch to a different working directory,
+    // and then return to the initial working directory.
+    int wd_fd = open(".", O_RDONLY);
+
+    if (wd_fd == -1) {
+        perror("open");
+        return 1;
+    }
+
+    int ret = chdir("..");
+
+    if (ret) {
+        perror("chdir");
+        return 1;
+    }
+
+    print_cwd();
+
+    ret = fchdir(wd_fd);
+
+    if (ret) {
+        perror("fchdir");
+        return 1;
+    }
+
+    print_cwd();
+
+    ret = close(wd_fd);
+
+    if (ret) {
+        perror("close");
+        return 1;
+    }
+}
+
+void print_cwd() {
     char cwd[BUFSIZ];
 
     if (!getcwd(cwd, BUFSIZ)) {
         perror("getcwd");
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
-    printf("Current Working Directory: %s", cwd);
+    printf("Current Working Directory: %s\n", cwd);
 }
