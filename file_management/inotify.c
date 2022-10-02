d132d4b705abafdb08316a9d574f81ebcc2eb3a4
ajarombek <ajarombek95@gmail.com>
2022-10-02 17:40:07 -0400

    inotify example

--- file_management/inotify.c
+++ file_management/inotify.c
@@ -7,9 +7,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 
 void remove_inotify_watch(int fd, int wd);
 void create_file(char* filename);
+void read_inotify_events(int fd);
 
 int main() {
     int ret;
@@ -36,8 +39,10 @@ int main() {
     create_file("first.txt");
     create_file("second.txt");
 
+    read_inotify_events(fd);
+
     // Add a watch on the current directory.  Get events for read and writes of files.
-    wd = inotify_add_watch(fd, ".", IN_ACCESS | IN_MODIFY);
+    wd = inotify_add_watch(fd, ".", IN_OPEN | IN_ACCESS | IN_MODIFY);
 
     if (wd == -1) {
         perror("inotify_add_watch");
@@ -45,6 +50,34 @@ int main() {
     }
 
     printf("inotify watch descriptor for reads and writes: %d\n", wd);
+
+    // Open a file
+    int first_txt_fd = open("./first.txt", O_RDWR, 0644);
+
+    // Write to a file
+    const char *buf = "Hello";
+    ssize_t r;
+
+    while ((r = write(first_txt_fd, buf, strlen(buf))) != 0) {
+        if (r == -1) {
+            if (errno == EINTR)
+                continue;
+
+            perror("write");
+            break;
+        }
+
+        buf += r;
+    }
+
+    read_inotify_events(fd);
+
+    ret = close(first_txt_fd);
+
+    if (fd == -1) {
+        perror("close");
+        return 1;
+    }
 
     remove_inotify_watch(fd, wd);
 
@@ -76,4 +109,32 @@ void create_file(char* filename) {
     }
 
     printf("Created file %s with descriptor %d\n", filename, fd);
+}
+
+void read_inotify_events(int fd) {
+    char buf[BUFSIZ];
+    ssize_t len = read(fd, buf, BUFSIZ);
+    ssize_t i = 0;
+
+    while (i < len) {
+        struct inotify_event *event = (struct inotify_event*) &buf[i];
+        printf("event wd = %d\n", event->wd);
+        printf("event mask = %d\n", event->mask);
+        printf("event cookie = %d\n", event->cookie);
+        printf("event len = %d\n", event->len);
+
+        if (event->len)
+            printf("event name = %s\n", event->name);
+
+        if (event->mask & IN_CREATE)
+            printf("event type: a file was created\n");
+        if (event->mask & IN_OPEN)
+            printf("event type: a file was opened\n");
+        if (event->mask & IN_ACCESS)
+            printf("event type: a file was read\n");
+        if (event->mask & IN_MODIFY)
+            printf("event type: a file was modified\n");
+
+        i += sizeof(struct inotify_event*) + event->len;
+    }
 }
